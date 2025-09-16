23f38339257321d7f1c362908462b5cbad2f0f09
Plata101 <marc.friedmann@yahoo.com>
2025-09-16 11:41:04 +0200

    impact sound, walls, brick destruction

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,6 +2,8 @@ extends RigidBody2D
 
 @export var speed = 400.0
 
+@onready var ping_sound = $PingSound
+
 func _ready():
 	# Make sure Contact Monitor is on and Max Contacts Reported is > 0
 	# in the Inspector if you need to detect collisions in the script.
@@ -12,10 +14,9 @@ func _ready():
 # That's it! No _physics_process is needed for movement.
 # The physics engine handles all bouncing and motion.
 
-
-
 func _on_body_entered(body):
-	print("hit something")
+	ping_sound.play()
 	# Check if the body the ball hit is in the "bricks" group
 	if body.is_in_group("bricks"):
-		body.take_damage
+		print("brick")
+		body.take_damage()
--- scripts/brick.gd
+++ scripts/brick.gd
@@ -5,7 +5,7 @@ signal destroyed(points)
 
 # Create slots in the Inspector to drag your textures into.
 @export var solid_texture: Texture2D
-@export var damagaed_texture: Texture2D
+@export var damaged_texture: Texture2D
 
 var health = 2
 const POINTS = 100
@@ -15,9 +15,13 @@ const POINTS = 100
 func _ready():
 	# Start by showing the solid brick texture.
 	sprite.texture = solid_texture
+	
+func take_damage():
+	health -= 1
+	print("health:", health)
 	if health == 1:
 		# if health is 1, it's damaged
-		sprite.texture = damagaed_texture
+		sprite.texture = damaged_texture
 		# TODO: add crack sound
 	elif health <= 0:
 		destroyed.emit(POINTS) # Emit the signal with the point value.
