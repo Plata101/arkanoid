083dd974cff0657c993068aa40f9ab91bacbe602
Plata101 <marc.friedmann@yahoo.com>
2025-09-18 10:01:04 +0200

    ball bouncing logic added

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,8 +1,15 @@
 extends RigidBody2D
 
 @export var speed = 400.0
-
+@export var max_paddle_angle = 1.2
+# This new variable controls how much the paddle's movement affects the ball.
+# A value of 0.3 means 30% of the paddle's speed is transferred.
+@export var paddle_influence = 0.3
 @onready var ping_sound = $PingSound
+# Add this new variable. It's the minimum angle from horizontal (in degrees).
+# 15 is a good starting value.
+@export var min_bounce_angle_degrees = 15.0
+
 
 func _ready():
 	# Make sure Contact Monitor is on and Max Contacts Reported is > 0
@@ -17,11 +24,36 @@ func _ready():
 func _on_body_entered(body):
 	ping_sound.play()
 	
-	
-	
 	if body.is_in_group("paddle"):
-		# --- CLASSIC ARKANOID BOUNCE LOGIC ---
-		print("hit the paddle")
+		# 1. Calculate base velocity from hit location and paddle movement
+		var ball_pos_x = self.global_position.x
+		var paddle_pos_x = body.global_position.x
+		var paddle_width = body.paddle_width
+		var influence = (ball_pos_x - paddle_pos_x) / (paddle_width / 2.0)
+		var new_velocity = Vector2.UP.rotated(influence * max_paddle_angle)
+		new_velocity.x += body.velocity.x * paddle_influence
+		
+		# --- ANGLE CLAMPING LOGIC ---
+		
+		# 2. Convert the minimum angle from degrees to radians for calculations
+		var min_bounce_angle_rad = deg_to_rad(min_bounce_angle_degrees)
+		
+		# 3. Get the final direction's angle (in radians)
+		var angle = new_velocity.angle()
+		
+		# 4. Clamp the angle to ensure it's not too horizontal
+		# The range from -PI/2 is straight up. We clamp it to be within our min angle.
+		if abs(angle + PI / 2) > max_paddle_angle:
+			angle = -PI / 2 + sign(angle) * max_paddle_angle
+		
+		# Ensure the ball doesn't go too horizontal
+		if angle > -min_bounce_angle_rad: # Too close to the right horizontal
+			angle = -min_bounce_angle_rad
+		elif angle < -PI + min_bounce_angle_rad: # Too close to the left horizontal
+			angle = -PI + min_bounce_angle_rad
+			
+		# 5. Apply the corrected direction and speed
+		self.linear_velocity = Vector2.from_angle(angle) * speed
 	
 	# Check if the body the ball hit is in the "bricks" group
 	if body.is_in_group("bricks"):
--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,25 +1,25 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @export var speed = 300.0 # You can change this speed in the Inspector
 @onready var playerPaddle = $AnimatedSprite2D
+@export var paddle_width: float = 110.0
 
-var lock_position
+var lock_position_y: float
+
 
 func _ready():
 	playerPaddle.play("default")
-	lock_position = position.y
+	# Save the initial Y position
+	lock_position_y = self.position.y
 
 func _physics_process(delta):
 	# Get the player's input direction
 	var direction = Input.get_axis("moveLeft", "moveRight")
-	
-	# Create a velocity vector for this frame
-	var velocity = Vector2.ZERO
 	velocity.x = direction * speed
 	
-	# The key change:
-	# Move the kinematic body by the desired amount for this frame.
-	# We multiply by 'delta' here to make the movement frame-rate independent.
-	move_and_collide(velocity * delta)
-	position.y = lock_position
+	# Let Godot handle the movement and collisions
+	move_and_slide()
+	
+	# Forcefully correct any unwanted vertical movement
+	self.position.y = lock_position_y
 	
